e40b84e810555150ecf01292e8009e996c539f11
Maydanik <tmaydanik@gmail.com>
2019-06-15 13:15:35 +0300

    add componentDidMount

--- src/pages/coins/Coins.jsx
+++ src/pages/coins/Coins.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import AllCoins from './coinsList.json';
+// import AllCoins from './coinsList.json';
 import HeaderComponent from '../../components/Header';
 import SearchComponent from '../../components/Search';
 import CoinsList from '../../components/CoinsList';
@@ -7,9 +7,25 @@ import './Coins.css';
 
 class Coins extends Component {
     state = {
-        coinsList: Object.keys(AllCoins.Data).slice(0, 21).map(key => AllCoins.Data[key]),
+        // coinsList: Object.keys(AllCoins.Data).slice(0, 21).map(key => AllCoins.Data[key]),
+        coinsList: [],
         search: '',
     };
+
+    componentDidMount() {
+            fetch('https://min-api.cryptocompare.com/data/all/coinlist', {
+                method: 'GET',
+            })
+                .then((response) => {
+                    return response.json();
+                })
+                .then((AllCoins) => {
+                    this.setState({coinsList: Object.keys(AllCoins.Data).slice(0, 21).map(key => AllCoins.Data[key])});
+                })
+                .catch((err) => {
+                    console.log(err);
+                })
+        };
 
     handlerSearchChange = (event) => {
         this.setState({ search: event.target.value });
