d7095a9a5f2eaa103d96aa859bb56309eb2eb25d
Maydanik <tmaydanik@gmail.com>
2019-06-28 18:52:13 +0300

    fix components

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -52,21 +52,3 @@ class SearchComponent extends Component {
 }
 
 export default SearchComponent;
-
-// import React from 'react';
-
-// const SearchComponent = (props) => (
-//     <div>
-//         <span className = 'search'>Search</span>
-//         <input
-//             type='text'
-//             name='search'
-//             id='search'
-//             placeholder='type to search'
-//             value={props.value}
-//             onChange={props.onChange}>
-//         </input>
-//     </div>
-// );
-
-// export default SearchComponent;
